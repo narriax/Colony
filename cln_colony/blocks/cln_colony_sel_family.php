fcb60542441ae5d03c176abc17ffc499fe5383d5
Narriax <narriax@gmail.com>
2015-05-05 00:02:31 -0400

    basic page

---
+++ cln_colony/blocks/cln_colony_sel_family.php
@@ -0,0 +1,6 @@
+<?php 
+
+
+function cln_colony_block_sel_family () {
+	return 'im a block';
+}
---
+++ cln_colony/pages/colony.php
@@ -0,0 +1,7 @@
+<?php
+
+function cln_colony_pages_colony() {
+	module_load_include ('class', 'cln_colony', 'api/cln_chars');
+
+	return '';
+}
